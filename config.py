73359162a5b1013939153b16b0d8a5114a07484b
安稚初 <xiaoxinnbxiaoxin@gmail.com>
2026-03-13 13:15:12 +0800

    创建 config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+# Telegram机器人核心配置
+# 机器人Token
+BOT_TOKEN = "机器人token"
+
+# 频道验证相关配置
+REQUIRED_CHANNEL = "@mtproxysv"
+REQUIRED_CHANNEL_ID = -1003663591656
+CHANNEL_LINK = "https://t.me/mtproxysv"
+
+# 管理员配置（唯一管理员ID）
+ADMIN_ID = 5394169772
+
+# AI接口配置
+AI_API_KEY = "接口密钥"
+AI_MODEL = "glm-4-flash" #模型
+
+# 远程MySQL数据库配置（已填你的服务器信息）
+MYSQL_HOST = ""   #数据库地址 可以本地
+MYSQL_PORT = 3306
+MYSQL_USER = ""
+MYSQL_PWD = ""
+MYSQL_DB = ""
